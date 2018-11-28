adf172f7df9ed6d9c6924dd824c8560bc1344e63
chrissydc <bychriscelle@gmail.com>
2018-11-27 20:00:27 -0500

    animations are more seamless between transitions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 window.addEventListener("keydown", function(e) {
-    this.console.log(e);
+    console.log(e);
+    // for audio to play
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    // for animation to take place when key has been selected
+    const key = document.querySelector(`.keys[data-key="${e.keyCode}"]`);
+
     this.console.log(audio);
     if (!audio) return;
 
@@ -8,4 +12,15 @@ window.addEventListener("keydown", function(e) {
     audio.currentTime = 0;
     // execute play method on audio
     audio.play();
-})
+    key.classList.add("played");
+});
+
+const keys = document.querySelectorAll(".keys");
+
+function removeTransition(e) {
+    if (e.propertyName != "transform") return;
+    this.classList.remove("played")
+}
+
+// loop on every element to add an eventListener
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
